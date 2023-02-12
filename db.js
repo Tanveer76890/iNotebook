905df3ca65f24d8573b5ef424eb18e57b8b3b5e0
Muhammad Tanveer <mtanveer76890@gmail.com>
2023-02-12 17:35:34 +0500

    some also changes for gitignore

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const mongoURI = MONGOURI;
 
 // const mongoURI = 'mongodb://localhost:27017/?directConnection=true&readPreference=primary'
 
-mongoose.set('strictQuery', false);
+// mongoose.set('strictQuery', false);
 
 const connectToMongo = ()=>{
      mongoose.connect(mongoURI, ()=>{
